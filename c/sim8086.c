d509ac6a27f55f1082216478b24fec4762cd71e6
Danylo Lytovchenko <danylo.lytovchenko@pm.me>
2024-07-31 04:54:53 +0200

    Add memory calculation disassembly

--- c/sim8086.c
+++ c/sim8086.c
@@ -28,28 +28,62 @@ int main(int argc, char *argv[]) {
   uint8_t instruction[6];
   char *w[2][8] = {{"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"},
                    {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"}};
+  char *mods[8] = {"bx + si", "bx + di", "bp + si", "bp + di",
+                   "si",      "di",      "bp",      "bx"};
   fread(instruction, 1, 1, program);
   while (feof(program) == 0) {
     if (ferror(program) != 0) {
       perror("Failed to read byte");
       return EXIT_FAILURE;
     }
-    if (instruction[0] >> 2 == 0b100010) { // register to register
+    if (instruction[0] >> 2 == 0b100010) { // register/memory to/from register
       fread(&instruction[1], 1, 1, program);
-      if ((instruction[1] >> 6 & 0b11) == 0b11) {
-        char *reg;
-        char *rm;
-
-        reg = w[instruction[0] & 1][(instruction[1] & 0b00111111) >> 3];
-        rm = w[instruction[0] & 1][instruction[1] & 0b00000111];
+      int mod = instruction[1] >> 6;
+      char *reg = w[instruction[0] & 1][(instruction[1] & 0b00111111) >> 3];
+      char *rm = w[instruction[0] & 1][instruction[1] & 0b00000111];
+      printf("mod is %b\n", mod);
+      if (mod == 0b11) {
         if ((instruction[0] >> 1 & 1)) {
           fprintf(disassemble, "mov %s, %s\n", reg, rm);
         } else {
           fprintf(disassemble, "mov %s, %s\n", rm, reg);
         }
-      } else {
-        puts("Not handling that yet");
-        // return EXIT_FAILURE;
+      } else if (mod == 0b00) {
+        if ((instruction[0] >> 1 & 1))
+          fprintf(disassemble, "mov %s, [%s]\n", reg,
+                  mods[instruction[1] & 0b00000111]);
+        else
+          fprintf(disassemble, "mov [%s], %s\n",
+                  mods[instruction[1] & 0b00000111], reg);
+
+      } else if (mod == 0b01) {
+        fread(&instruction[2], 1, 1, program);
+        uint8_t displacement = instruction[2];
+        if ((instruction[0] >> 1 & 1)) {
+          if (displacement != 0)
+            fprintf(disassemble, "mov %s, [%s + %d]\n", reg,
+                    mods[instruction[1] & 0b00000111], instruction[2]);
+          else
+            fprintf(disassemble, "mov %s, [%s]\n", reg,
+                    mods[instruction[1] & 0b00000111]);
+        } else {
+          if (displacement != 0)
+            fprintf(disassemble, "mov [%s + %d], %s\n",
+                    mods[instruction[1] & 0b00000111], instruction[2], reg);
+          else
+            fprintf(disassemble, "mov [%s], %s\n",
+                    mods[instruction[1] & 0b00000111], reg);
+        }
+      } else if (mod == 0b10) {
+        fread(&instruction[2], 1, 2, program);
+        if ((instruction[0] >> 1 & 1))
+          fprintf(disassemble, "mov %s, [%s + %d]\n", reg,
+                  mods[instruction[1] & 0b00000111],
+                  instruction[2] | instruction[3] << 8);
+        else
+          fprintf(disassemble, "mov [%s + %d], %s\n",
+                  mods[instruction[1] & 0b00000111],
+                  instruction[2] | instruction[3] << 8, reg);
       }
     } else if (instruction[0] >> 1 ==
                0b1100011) { // Immediate to register/memory
@@ -65,7 +99,7 @@ int main(int argc, char *argv[]) {
       if (width) {
         data |= (instruction[2] << 8);
       }
-      fprintf(disassemble, "mov %s %d\n", reg, data);
+      fprintf(disassemble, "mov %s, %d\n", reg, data);
     } else {
       puts("ILLEGAL INSTRUCTION");
       return EXIT_FAILURE;
