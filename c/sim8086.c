2f55ce8884362ae1387bf86d177e9b2b9563ddba
Danylo Lytovchenko <danylo.lytovchenko@pm.me>
2024-07-31 04:07:13 +0200

    Add 8-bit immediate mov disassemboly

--- c/sim8086.c
+++ c/sim8086.c
@@ -25,16 +25,17 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  uint8_t instruction[2];
+  uint8_t instruction[6];
   char *w[2][8] = {{"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"},
                    {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"}};
-  fread(instruction, 1, 2, program);
+  fread(instruction, 1, 1, program);
   while (feof(program) == 0) {
     if (ferror(program) != 0) {
       perror("Failed to read byte");
       return EXIT_FAILURE;
     }
-    if ((instruction[0] >> 2 & MOV_OPCODE) == MOV_OPCODE) {
+    if (instruction[0] >> 2 == 0b100010) { // register to register
+      fread(&instruction[1], 1, 1, program);
       if ((instruction[1] >> 6 & 0b11) == 0b11) {
         char *reg;
         char *rm;
@@ -48,13 +49,30 @@ int main(int argc, char *argv[]) {
         }
       } else {
         puts("Not handling that yet");
-        return EXIT_FAILURE;
+        // return EXIT_FAILURE;
+      }
+    } else if (instruction[0] >> 1 ==
+               0b1100011) { // Immediate to register/memory
+      size_t next_read = 4 + (instruction[0] & 1);
+      fread(&instruction[1], 1, next_read, program);
+      // TODO: implement
+    } else if (instruction[0] >> 4 == 0b1011) { // immediate to register
+      uint8_t width = ((instruction[0] >> 3) & 1);
+      size_t next_read = 1 + width;
+      fread(&instruction[1], 1, next_read, program);
+      char *reg = w[width][(instruction[0] & 0b00000111)];
+      if (width) {
+        uint16_t data = instruction[1] & (instruction[2] << 8);
+        fprintf(disassemble, "mov %s %d\n", reg, data);
+      } else {
+        fprintf(disassemble, "mov %s, %d\n", reg, instruction[1]);
       }
     } else {
-      puts("ILLEGAL instruction");
+      puts("ILLEGAL INSTRUCTION");
       return EXIT_FAILURE;
     }
-    fread(instruction, 1, 2, program);
+
+    fread(instruction, 1, 1, program);
   }
   return EXIT_SUCCESS;
 }
